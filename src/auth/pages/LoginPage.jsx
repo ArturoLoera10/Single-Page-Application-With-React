cf633a7eccc8a52799fa7f8a55afdc4698658ae2
ArturoLoera10 <loeraarturo26@gmail.com>
2024-08-03 15:01:09 -0600

    Login and Logout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context";
 
 export const LoginPage = () => {
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const onLogin = () => {
+    login("Fernando H");
+
     navigate("/", {
       replace: true,
     });
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth";
+import { useContext } from "react";
 
 export const Navbar = () => {
+  const { user, logout } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const onLogout = () => {
+    logout();
+
     navigate("/login", {
       replace: true,
     });
@@ -48,7 +53,7 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-primary">Fernando</span>
+          <span className="nav-item nav-link text-primary">{user?.name}</span>
 
           <button onClick={onLogout} className="nav-item nav-link btn">
             Logout
